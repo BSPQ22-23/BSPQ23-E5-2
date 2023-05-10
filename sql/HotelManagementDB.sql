cb889c7149259bae2171af89023fd2f48c4632bc
a-rasines <alberto.rasines@opendeusto.es>
2023-05-10 10:47:11 +0200

    Test for github actions

--- sql/HotelManagementDB.sql
+++ sql/HotelManagementDB.sql
@@ -1,12 +1,5 @@
-
-/* DELETE 'HotelManagementDB' database*/
-DROP SCHEMA IF EXISTS HotelManagementDB;
-/* DELETE USER 'strava_user' AT LOCAL SERVER*/
+DROP SCHEMA IF EXISTS HotelManager;
 DROP USER IF EXISTS 'spq'@'localhost';
-
-/* CREATE 'stravadb' DATABASE */
-CREATE SCHEMA IF NOT EXISTS HotelManagementDB;
-/* CREATE THE USER 'strava_user' AT LOCAL SERVER WITH PASSWORD 'strava_user' */
+CREATE SCHEMA IF NOT EXISTS HotelManager;
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-/* GRANT FULL ACCESS TO THE DATABASE 'HotelManagementDB' FOR THE USER 'strava_user' AT LOCAL SERVER*/
-GRANT ALL ON  HotelManagementDB.* TO 'sd'@'localhost';
+GRANT ALL ON HotelManager.* TO 'spq'@'localhost';
